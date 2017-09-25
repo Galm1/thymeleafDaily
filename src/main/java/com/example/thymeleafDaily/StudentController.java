b3e23bdd4b57eec7f681fd7de3a98a720dc89123
EliGarcia <garciaelisha93@yahoo.com>
2017-09-25 14:43:57 -0500

    complete

--- src/main/java/com/example/thymeleafDaily/StudentController.java
+++ src/main/java/com/example/thymeleafDaily/StudentController.java
@@ -13,4 +13,24 @@ public class StudentController {
         model.addAttribute("grades", Grade.values());
         return "new_student";
     }
+    @RequestMapping("/create_student")
+    public String createStudent(@RequestParam(value="first-name") String firstName, @RequestParam(value="last-name") String lastName, @RequestParam(value="grade") Grade grade, Model model) {
+        Student student = new Student();
+
+        System.out.println("Student Info....");
+
+        System.out.println("First name: "+firstName);
+        student.setFirstName(firstName);
+
+        System.out.println("Last name: "+lastName);
+        student.setLastName(lastName);
+
+        System.out.println("Grade: "+ grade);
+        student.setGrade(grade);
+
+        model.addAttribute("student", student);
+
+        return "view_student";
+
+    }
 }
