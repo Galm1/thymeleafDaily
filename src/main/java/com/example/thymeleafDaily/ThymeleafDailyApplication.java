42c8b8144978ea5db5945e4ccb159623df3b6475
EliGarcia <garciaelisha93@yahoo.com>
2017-09-25 13:36:49 -0500

    first commit

---
+++ src/main/java/com/example/thymeleafDaily/ThymeleafDailyApplication.java
@@ -0,0 +1,12 @@
+package com.example.thymeleafDaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafDailyApplication.class, args);
+	}
+}
